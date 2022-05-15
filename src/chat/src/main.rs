6d712c8899e5ada985c80911750a18eacd9d55c0
Tetsurou Nishioka <>
2022-05-15 20:57:44 +0900

    About chat. Comm worked

--- src/chat/src/main.rs
+++ src/chat/src/main.rs
@@ -24,40 +24,62 @@ fn server(ip: &String, port:u32) {
             Ok(mut stream) => {
                 thread::spawn( // 別スレッドを発生させて引数の関数を実行する (spawn:発生させる)
                     move || { // moveで、ラムダ（クロージャ）関数に渡す引数の所有権譲渡を強制する
-                        let mut buf: [u8; 512] = [0; 512]; // 512 byte buffer formatted with 0
-                        let byte_read = stream.read(&mut buf).expect("Faild to read_exact"); //&mutをつけて、msgを参照かつ編集可能にする
-                        let rcvd = from_utf8(&buf).expect("Faild to read_exact"); // uint8型を文字列として解釈
-                        println!("rcvd:{}",rcvd);                    
+                        loop{
+                            let mut buf: [u8; 512] = [0; 512]; // 512 byte buffer formatted with 0
+                            let byte_read = stream.read(&mut buf).expect("Faild to read_exact"); //&mutをつけて、msgを参照かつ編集可能にする
+                            let rcvd = from_utf8(&buf).expect("Faild to read_exact"); // uint8型を文字列として解釈
+                            println!("rcvd:{}",rcvd);                    
+                        }
                     }
                 );
             }
         }
     }
-
-
 }
 
 fn client(ip: &String, port:u32) {
     println!("client ip:{} port:{}",ip,port);
     let addr = ip.to_string() + ":" + &port.to_string();
 
-    if let Ok(mut stream) = TcpStream::connect(addr) {
-        loop {
-            // Read user key input
-            let mut user_keyinput = String::new();
-            io::stdin()
-                .read_line(&mut user_keyinput)
-                .expect("Failed to readline");
-
-            let input = &user_keyinput;
-
-            match stream.write(input.as_bytes()) { 
-              Ok(rslt) => { println!("Sent {} {}",rslt,input); } 
-              Err(e) => { panic!("{}",e); }
-            } // ?をつけることで、write内部でpanicが起こった場合はErrを返してくれる
+    match TcpStream::connect(addr) {
+        Ok(mut stream) => {
+            loop {
+                // Read user key input
+                let mut user_keyinput = String::new();
+                io::stdin()
+                    .read_line(&mut user_keyinput)
+                    .expect("Failed to readline");
+    
+                let input = &user_keyinput;
+    
+                match stream.write(input.as_bytes()) { 
+                  Ok(rslt) => { println!("Sent {} {}",rslt,input); } 
+                  Err(e) => { panic!("{}",e); }
+                } // ?をつけることで、write内部でpanicが起こった場合はErrを返してくれる
+            }
         }
-        // Ok(()); //これがないと”何も返さない関数”扱いされて、"?"オペランドでエラーになる
+        Err(e) => {
+            eprintln!("Connection failed by {}",e);
+        }
     }
+    
+    // if let Ok(mut stream) = TcpStream::connect(addr) {
+    //     loop {
+    //         // Read user key input
+    //         let mut user_keyinput = String::new();
+    //         io::stdin()
+    //             .read_line(&mut user_keyinput)
+    //             .expect("Failed to readline");
+
+    //         let input = &user_keyinput;
+
+    //         match stream.write(input.as_bytes()) { 
+    //           Ok(rslt) => { println!("Sent {} {}",rslt,input); } 
+    //           Err(e) => { panic!("{}",e); }
+    //         } // ?をつけることで、write内部でpanicが起こった場合はErrを返してくれる
+    //     }
+    //     // Ok(()); //これがないと”何も返さない関数”扱いされて、"?"オペランドでエラーになる
+    // }
 }
 
 fn main() {
